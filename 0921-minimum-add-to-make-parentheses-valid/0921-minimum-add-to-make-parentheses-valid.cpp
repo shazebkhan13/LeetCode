e67d184133df3e81d1dc5e571275239bbd0d423b
Shazeb Khan <shazeb26jan@gmail.com>
2026-07-16 18:19:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.53%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(st.size() && s[i]==')' && st.top()=='(') st.pop();
+            else st.push(s[i]);
+        }
+        return st.size();
+    }
+};
