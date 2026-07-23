815621a9f72dcd8c2add9754514e910b3a757bdc
Shazeb Khan <shazeb26jan@gmail.com>
2026-07-23 21:40:35 +0530

    Time: 33 ms (8.78%), Space: 43.6 MB (8.53%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -4,14 +4,14 @@ public:
         int n=s.size(),m=t.size();
         if(n==0) return true;
         if(n>m) return false;
-        vector<int> pre(n,-1),suf(n,m);
+        vector<int> pre(n,m),suf(n,-1);
         int p=0;
         for(int i=0;i<n;i++){
             while(p<m && s[i]!=t[p]) p++;
             if(p==m) break;
             pre[i]=p++;
         }
-        if(pre[n-1]!=-1) return true;
+        if(pre[n-1]!=m) return true;
         p=m-1;
         for(int i=n-1;i>=0;i--){
             while(p>=0 && s[i]!=t[p]) p--;
@@ -19,9 +19,9 @@ public:
             suf[i]=p--;
         }
         for(int i=0;i<n;i++){
-            int a=(i-1<0)?-1:pre[i-1];
-            int b=(i+1>=n)?m:suf[i+1];
-            if((i==0||a!=-1) && (i==n-1||b!=m) && b-a>=2) return true;
+            int a=(i==0)?-1:pre[i-1];
+            int b=(i==n-1)?m:suf[i+1];
+            if(a!=m && b!=-1 && b-a>=2) return true;
         }
         return false;
     }
