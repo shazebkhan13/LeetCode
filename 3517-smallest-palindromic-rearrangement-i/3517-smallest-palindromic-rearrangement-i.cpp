8a9838d0859959c2e69884ed0a97866c0cc477a3
Shazeb Khan <shazeb26jan@gmail.com>
2026-07-28 15:40:15 +0530

    Time: 218 ms (5.05%), Space: 56.3 MB (83.08%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        sort(s.begin(), s.begin() + n / 2);
+        sort(s.rbegin(), s.rbegin() + n / 2);
+        return s;
+    }
+};
