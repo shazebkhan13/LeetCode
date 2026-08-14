88091f8875f45a4d3cfce4124da254148b014928
Shazeb Khan <shazeb26jan@gmail.com>
2026-08-15 01:04:11 +0530

    Time: 17 ms (7.21%), Space: 24.3 MB (73.54%) - LeetHub

---
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPrime(int x){
+        if(x<2) return false;
+        for(int i=2;i<x;i++){
+            if(x%i==0) return false;
+        }
+        return true;
+    }
+    bool checkPrimeFrequency(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map<int,int> mp;
+        for(auto i:arr) mp[i]++;
+        for(auto i:mp){
+            cout<<i.first<<"->"<<i.second<<endl;
+            if(checkPrime(i.second)) return true;
+        }
+        return false;
+    }
+};
