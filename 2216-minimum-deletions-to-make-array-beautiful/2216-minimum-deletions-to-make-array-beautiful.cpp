32c195d0839f7f358f244eb826d338d813515f0c
Shazeb Khan <shazeb26jan@gmail.com>
2026-08-15 15:46:28 +0530

    Time: 3 ms (54.7%), Space: 124.8 MB (94.7%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int minDeletion(vector<int>& arr) {
-        int n=arr.size(),ind=0,i=0;
-        vector<int> ans;
-        while(i<n){
-            while(ind%2&&arr[i]==arr[i-1]){
-                i++;
-                if(i>=n) break;
-            }
-            if(i>=n) break;
-            ans.push_back(arr[i]);
-            ind++,i++;
+        int n=arr.size(),ans=0;
+        for(int i=0;i<n-1;i++){
+            if((i-ans)%2==0&&arr[i]==arr[i+1]) ans++;
         }
-        if(ans.size()%2) ans.pop_back();
-        return n-ans.size();
+        return ans+(n-ans)%2;
     }
 };
