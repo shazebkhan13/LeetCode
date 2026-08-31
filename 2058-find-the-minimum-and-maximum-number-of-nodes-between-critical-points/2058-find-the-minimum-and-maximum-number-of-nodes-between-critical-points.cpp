322ed88561b978e90bdd15a934ebcb67cd4dc0b2
Shazeb Khan <shazeb26jan@gmail.com>
2026-09-01 02:49:18 +0530

    Time: 8 ms (26.72%), Space: 124.5 MB (81.95%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -12,16 +12,18 @@ class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         ListNode *cur=head;
-        int ind=1;
+        int ind=1,first=INT_MIN,last=INT_MAX,mini=INT_MAX;
         vector<int> arr;
         while(cur->next->next){
-            if((cur->next->val>cur->val&&cur->next->val>cur->next->next->val)||(cur->next->val<cur->val&&cur->next->val<cur->next->next->val)) arr.push_back(ind);
+            if((cur->next->val>cur->val&&cur->next->val>cur->next->next->val)||(cur->next->val<cur->val&&cur->next->val<cur->next->next->val)){
+                if(first==INT_MIN) first=ind;
+                if(last!=INT_MAX) mini=min(mini,ind-last);
+                last=ind;
+            }
             ind++;
             cur=cur->next;
         }
-        if(arr.size()<2) return {-1,-1};
-        int mini=INT_MAX;
-        for(int i=0;i<arr.size()-1;i++) mini=min(mini,arr[i+1]-arr[i]);
-        return {mini,arr.back()-arr[0]};
+        if(mini==INT_MAX||first==INT_MIN||last==INT_MAX) return {-1,-1};
+        return {mini,last-first};
     }
 };
