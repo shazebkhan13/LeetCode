c9e321cdd254a3b13ce2e0dbafd0f257bfe36829
Shazeb Khan <shazeb26jan@gmail.com>
2026-09-12 01:51:13 +0530

    Time: 27 ms (18.41%), Space: 19.4 MB (50.97%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        unordered_map<char,int> mp;
+        int cnt=0;
+        for(auto i:s){
+            mp[i]++;
+            cnt++;
+        }
+        for(auto i:t) if(mp[i]){
+            mp[i]--;
+            cnt--;
+        }
+        return cnt;
+    }
+};
